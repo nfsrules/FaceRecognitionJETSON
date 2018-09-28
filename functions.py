c11205fe35b4759ff0fa6f20c73b9a01dbc0d7c8
Nelson Fernandez <nelson.fernandez@axionable.com>
2018-09-28 10:31:13 +0200

    initial commit

---
+++ functions.py
@@ -0,0 +1,118 @@
+
+import sys
+import numpy as np
+import cv2
+
+# Functions 
+def decode(net_output):
+    '''EmotionNet output decoder.
+    
+    '''
+    if (np.argmax(net_output) == 0):
+        prediction = 'Angry'
+    elif (np.argmax(net_output) == 1):
+        prediction = 'Disgust'
+    elif (np.argmax(net_output) == 2):
+        prediction = 'Fear'
+    elif (np.argmax(net_output) == 3):
+        prediction = 'Happy'
+    elif (np.argmax(net_output) == 4):
+        prediction = 'Sad'
+    elif (np.argmax(net_output) == 5):
+        prediction = 'Surprise'
+    else:
+        prediction = 'Neutral'
+        
+    return prediction
+        
+    
+def pre_processing(color_image_rectangle):
+    '''Transform input image to EmotionNet input size.
+    
+    '''
+    # Convert image to B&W
+    gray_img = cv2.cvtColor(color_image_rectangle,cv2.COLOR_BGR2GRAY)
+
+    # Resize image
+    gray_img = cv2.resize(gray_img,(48,48), interpolation = cv2.INTER_CUBIC)
+
+    # Reshape and add mini-batch dimension
+    gray_img = np.reshape(gray_img,(48,48,1))
+    gray_img = np.expand_dims(gray_img, axis=0)
+                      
+    return gray_img
+
+def gender_encoder(gender):
+    '''Gender label encoder --------------------------
+    
+    Male: 0
+    Female: 1
+    '''
+    if gender == 'm':
+        gender = 0
+    else:
+        gender = 1
+    return gender
+
+
+def gender_decoder(net_output, threshold=0.65):
+    '''GenderNet label decoder -----------------------
+
+    0: male
+    1: female
+    threshold: Probability minimum threshold to assign a label.
+    '''
+    if np.max(net_output) > threshold:
+        if np.argmax(net_output) == 0:
+            prediction = 'male'
+            probability = np.max(net_output)
+        else:
+            prediction = 'female'
+            probability = np.max(net_output)
+    else:
+        prediction = ''
+        probability = ''
+
+    return prediction
+                    
+# Tegra functions
+def open_cam_rtsp(uri, width, height, latency):
+    gst_str = ('rtspsrc location={} latency={} ! '
+               'rtph264depay ! h264parse ! omxh264dec ! '
+               'nvvidconv ! '
+               'video/x-raw, width=(int){}, height=(int){}, '
+               'format=(string)BGRx ! '
+               'videoconvert ! appsink').format(uri, latency, width, height)
+    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+
+
+def open_cam_usb(dev, width, height):
+    # We want to set width and height here, otherwise we could just do:
+    #     return cv2.VideoCapture(dev)
+    gst_str = ('v4l2src device=/dev/video{} ! '
+               'video/x-raw, width=(int){}, height=(int){}, '
+               'format=(string)RGB ! '
+               'videoconvert ! appsink').format(dev, width, height)
+    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+
+
+def open_cam_onboard(width, height):
+    # On versions of L4T prior to 28.1, add 'flip-method=2' into gst_str
+    gst_str = ('nvcamerasrc ! '
+               'video/x-raw(memory:NVMM), '
+               'width=(int)2592, height=(int)1458, '
+               'format=(string)I420, framerate=(fraction)30/1 ! '
+               'nvvidconv ! '
+               'video/x-raw, width=(int){}, height=(int){}, '
+               'format=(string)BGRx ! '
+               'videoconvert ! appsink').format(width, height)
+    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+
+
+def open_window(width, height):
+    cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(WINDOW_NAME, width, height)
+    cv2.moveWindow(WINDOW_NAME, 0, 0)
+    cv2.setWindowTitle(WINDOW_NAME, 'Camera Demo for Jetson TX2/TX1')
+
+
---
+++ main.py
@@ -0,0 +1,174 @@
+from functions import *
+import sys
+import argparse
+import cv2
+from keras.models import model_from_json, load_model
+import face_recognition
+
+
+WINDOW_NAME = 'CameraDemo'
+
+
+def parse_args():
+    # Parse input arguments
+    desc = 'Capture and display live camera video on Jetson TX2/TX1'
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('--rtsp', dest='use_rtsp',
+                        help='use IP CAM (remember to also set --uri)',
+                        action='store_true')
+    parser.add_argument('--uri', dest='rtsp_uri',
+                        help='RTSP URI, e.g. rtsp://192.168.1.64:554',
+                        default=None, type=str)
+    parser.add_argument('--latency', dest='rtsp_latency',
+                        help='latency in ms for RTSP [200]',
+                        default=200, type=int)
+    parser.add_argument('--usb', dest='use_usb',
+                        help='use USB webcam (remember to also set --vid)',
+                        action='store_true')
+    parser.add_argument('--vid', dest='video_dev',
+                        help='device # of USB webcam (/dev/video?) [1]',
+                        default=1, type=int)
+    parser.add_argument('--width', dest='image_width',
+                        help='image width [1920]',
+                        default=1920, type=int)
+    parser.add_argument('--height', dest='image_height',
+                        help='image height [1080]',
+                        default=1080, type=int)
+    args = parser.parse_args()
+    return args
+
+
+
+def read_cam(cap):
+    show_help = True
+    full_scrn = False
+    help_text = '"Esc" to Quit, "H" for Help, "F" to Toggle Fullscreen'
+    font = cv2.FONT_HERSHEY_PLAIN
+    while True:
+        if cv2.getWindowProperty(WINDOW_NAME, 0) < 0:
+            # Check to see if the user has closed the window
+            # If yes, terminate the program
+            break
+        _, frame = cap.read() # grab the next image frame from camera
+        if show_help:
+            cv2.putText(frame, help_text, (11, 20), font,
+                        1.0, (32, 32, 32), 4, cv2.LINE_AA)
+            cv2.putText(frame, help_text, (10, 20), font,
+                        1.0, (240, 240, 240), 1, cv2.LINE_AA)
+        
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]  # size (180, 320, 3)
+        
+        # Only process every other frame of video to save time
+        if process_this_frame:
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            
+            face_emotions = []
+            for face in face_locations:
+                
+                # Get face coordinates
+                top, right, bottom, left = face
+
+                # Crop image 
+                face_image = rgb_small_frame[top:bottom, left:right]
+                
+                # Pre-process image
+                gray_img = pre_processing(face_image)
+                
+                # Call model.predict() method
+                net_output = model.predict(gray_img)
+                net2_output = gender_model.predict(gray_img)
+                
+                # Decode output
+                prediction = decode(net_output)
+                gender_prediction = gender_decoder(net2_output)
+                
+                # Add emotion to rectangle name
+                face_emotions.append(prediction + '\n' + gender_prediction)
+
+        process_this_frame = not process_this_frame
+
+        # Display the results
+        for (top, right, bottom, left), emotion in zip(face_locations, face_emotions):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom + 50), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, emotion, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        # Display the resulting image
+        cv2.imshow(WINDOW_NAME, frame)
+        #cv2.imshow(WINDOW_NAME, frame)
+        key = cv2.waitKey(10)
+        if key == 27: # ESC key: quit program
+            break
+        elif key == ord('H') or key == ord('h'): # toggle help message
+            show_help = not show_help
+        elif key == ord('F') or key == ord('f'): # toggle fullscreen
+            full_scrn = not full_scrn
+            if full_scrn:
+                cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN,
+                                      cv2.WINDOW_FULLSCREEN)
+            else:
+                cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN,
+                                      cv2.WINDOW_NORMAL)
+
+
+def main():
+
+    gender_model = load_model('Models/trained_GenderNet.hdf5')
+    # load json and create model
+    json_file = open('Models/face_model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+
+    # load weights into new model
+    loaded_model.load_weights("./Trained Model/face_model.h5")
+    print("Loaded model from disk")
+    model = loaded_model
+
+    args = parse_args()
+    print('Called with args:')
+    print(args)
+    print('OpenCV version: {}'.format(cv2.__version__))
+
+    if args.use_rtsp:
+        cap = open_cam_rtsp(args.rtsp_uri,
+                            args.image_width,
+                            args.image_height,
+                            args.rtsp_latency)
+    elif args.use_usb:
+        cap = open_cam_usb(args.video_dev,
+                           args.image_width,
+                           args.image_height)
+    else: # by default, use the Jetson onboard camera
+        cap = open_cam_onboard(args.image_width,
+                               args.image_height)
+
+    if not cap.isOpened():
+        sys.exit('Failed to open camera!')
+
+    open_window(args.image_width, args.image_height)
+    read_cam(cap)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main'
+
+
+
+
